cfec128edc074d8e98133db9fe7df26eef030904
xing_qixin <752154173@qq.com>
2019-08-27 10:02:03 +0800

    表单验证配置全部完成

--- src/main.js
+++ src/main.js
@@ -6,13 +6,17 @@ import store from './store'
 // 引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
 // less 的引入要在vant之后
 import './styles/index.less'
-// 使用表单验证
-// import VeeValidate from 'vee-validate'
-
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 Vue.use(Vant)
-// Vue.use(VeeValidate)
+
+Vue.use(VeeValidate, {
+  events: '' // 禁用事件触发验证,只能是
+})
+Validator.localize('zh_CN', zhCN)
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
+  <div>
     <van-nav-bar title="登录"/>
     <van-cell-group>
-         <!-- <van-field
+         <van-field
             v-model="user.mobile"
             label="手机号"
             placeholder="请输入手机号"
             v-validate="'required'"
             name='mobile'
-            :error-message="errors.first('email')"
+            :error-message="errors.first('mobile')"
         />
         <van-field
             v-model="user.code"
@@ -18,9 +18,9 @@
             v-validate="'required'"
             name='code'
             :error-message="errors.first('code')"
-        /> -->
+        />
 
-         <van-field
+         <!-- <van-field
             v-model="user.mobile"
             label="手机号"
             placeholder="请输入手机号"
@@ -29,15 +29,15 @@
         <van-field
             v-model="user.code"
             type="password"
-            label="密码"
+            label="验证码"
             name='code'
-            placeholder="请输入密码"
-        />
+            placeholder="请输入验证码"
+        /> -->
     </van-cell-group>
     <div class="loginBut">
-        <van-button type="info" square size="large" @click="loginOn">登录</van-button>
+        <van-button type="info" square size="large" :loading="loginInfo" @click="loginOn">登录</van-button>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -55,18 +55,26 @@ export default {
         //   接口文档要求的两个字段
         mobile: '13911111111',
         code: '246810'
-      }
+      },
+      loginInfo: false
     }
+  },
+  created () {
+    this.customValidateMessage()
+    // this.$validator.localize('zh_CN', dict)
   },
   methods: {
     async loginOn () {
       try {
+        // try 和catch的异常处理
         // // 表单验证
-        // const isValid = await this.$validator.validate()
-        // if (!isValid) {
-        //   return
-        // }
+        const isValid = await this.$validator.validate()
+        if (!isValid) {
+          return
+        }
+        this.loginInfo = true
         const { data } = await login(this.user)
+        this.loginInfo = false
         // 发请求
         console.log(data)
       } catch (err) {
@@ -74,8 +82,22 @@ export default {
         if (err.response && err.response.status === 400) {
           // toast.fail 是轻提示的书写方式是vant里面的方法
           this.$toast.fail('手机号或验证码错误')
+          this.loginInfo = false
         }
       }
+    },
+    customValidateMessage () {
+      this.$validator.localize('zh_CN', {
+        custom: {
+          // mobile 是要验证的字段的名字
+          mobile: {
+            required: '手机号不能为空'
+          },
+          code: {
+            required: '验证码不能为空'
+          }
+        }
+      })
     }
   }
 }
